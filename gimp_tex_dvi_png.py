e6a18428502bad405f0c5c623d3b40d938510613
Ryan Krauss <ryanlists@gmail.com>
2010-01-25 11:23:50 -0600

    added the ability to cleanly import jpegs with autocropping onto a lecture slide.  Also unified the adding of png, jpg, and pdf to all use the same underlying code.

--- gimp_tex_dvi_png.py
+++ gimp_tex_dvi_png.py
@@ -230,9 +230,56 @@ def scale_image(img, max_w, max_h, debug=0):
     return img
 
     
-def copy_png_to_img(png_path, img, x_offset=None, y_offset=None, \
-                    autocrop=True):
-    img2 = pdb.gimp_file_load(png_path, png_path)
+## def copy_png_to_img(png_path, img, x_offset=None, y_offset=None, \
+##                     autocrop=True):
+##     img2 = pdb.gimp_file_load(png_path, png_path)
+##     if autocrop:
+##         pdb.plug_in_autocrop(img2, img2.layers[0])
+##     w = img.width
+##     max_w = w-50
+##     h = img.height
+##     max_h = h-50
+##     if x_offset is not None:
+##         max_w -= x_offset
+##     if y_offset is not None:
+##         max_h -= y_offset
+##     scale_image(img2, max_w, max_h)
+##     #print('max_w=%s' % max_w)
+## ##     if (img2.width > max_w) or (img2.height > max_h):
+## ##         s1 = img2.width/max_w
+## ##         s2 = img2.height/max_h
+## ##         ar = float(img2.height)/float(img2.width)
+## ##         new_h = max_w*ar
+## ##         pdb.gimp_image_scale(img2, max_w, new_h)
+##     pdb.gimp_edit_copy_visible(img2)
+##     #gimp.Display(img2)
+##     width = img.width
+##     height = img.height
+##     #print('width = %s' % width)
+##     #print('height = %s' % height)
+##     if top_layer_is_TEMP(img):
+##         trans_layer = img.layers[0]
+##     else:
+##         trans_layer = gimp.Layer(img, 'Latex', width, height, \
+##                                  RGBA_IMAGE, 100, NORMAL_MODE)
+##         img.add_layer(trans_layer)
+##     float_layer = pdb.gimp_edit_paste(trans_layer, 1)
+##     if (x_offset is not None) or (y_offset is not None):
+##         if x_offset is None:#if either is not None, the other defaults
+##                             #to 0
+##             x_offset = 0
+##         if y_offset is None:
+##             y_offset = 0
+##         pdb.gimp_layer_set_offsets(float_layer, x_offset, y_offset)
+##     else:
+##         mcmd = "xdotool key M"
+##         time.sleep(0.1)
+##         os.system(mcmd)
+##     return float_layer
+
+
+def copy_img2_to_img(img2, img, x_offset=None, y_offset=None, \
+                     autocrop=True):
     if autocrop:
         pdb.plug_in_autocrop(img2, img2.layers[0])
     w = img.width
@@ -244,13 +291,6 @@ def copy_png_to_img(png_path, img, x_offset=None, y_offset=None, \
     if y_offset is not None:
         max_h -= y_offset
     scale_image(img2, max_w, max_h)
-    #print('max_w=%s' % max_w)
-##     if (img2.width > max_w) or (img2.height > max_h):
-##         s1 = img2.width/max_w
-##         s2 = img2.height/max_h
-##         ar = float(img2.height)/float(img2.width)
-##         new_h = max_w*ar
-##         pdb.gimp_image_scale(img2, max_w, new_h)
     pdb.gimp_edit_copy_visible(img2)
     #gimp.Display(img2)
     width = img.width
@@ -278,46 +318,23 @@ def copy_png_to_img(png_path, img, x_offset=None, y_offset=None, \
     return float_layer
 
 
-def copy_img2_to_img(img2, img, x_offset=None, y_offset=None):
-    w = img.width
-    max_w = w-50
-    if x_offset is not None:
-        max_w -= x_offset
-    #print('max_w=%s' % max_w)
-    if img2.width > max_w:
-        ar = float(img2.height)/float(img2.width)
-        new_h = max_w*ar
-        pdb.gimp_image_scale(img2, max_w, new_h)
-    pdb.gimp_edit_copy_visible(img2)
-    #gimp.Display(img2)
-    width = img.width
-    height = img.height
-    #print('width = %s' % width)
-    #print('height = %s' % height)
-    if top_layer_is_TEMP(img):
-        trans_layer = img.layers[0]
-    else:
-        trans_layer = gimp.Layer(img, 'Latex', width, height, \
-                                 RGBA_IMAGE, 100, NORMAL_MODE)
-        img.add_layer(trans_layer)
-    float_layer = pdb.gimp_edit_paste(trans_layer, 1)
-    if (x_offset is not None) or (y_offset is not None):
-        if x_offset is None:#if either is not None, the other defaults
-                            #to 0
-            x_offset = 0
-        if y_offset is None:
-            y_offset = 0
-        pdb.gimp_layer_set_offsets(float_layer, x_offset, y_offset)
-    else:
-        mcmd = "xdotool key M"
-        time.sleep(0.1)
-        os.system(mcmd)
-    return float_layer
-
-
-def copy_pdf_to_img(pdf_path, img, x_offset=None, y_offset=None):
+def copy_pdf_to_img(pdf_path, img, x_offset=None, y_offset=None, \
+                    autocrop=True):
     img2 = pdb.file_pdf_load(pdf_path, pdf_path)
-    copy_img2_to_img(img2, img, x_offset=x_offset, y_offset=y_offset)
+    return copy_img2_to_img(img2, img, x_offset=x_offset, y_offset=y_offset, \
+                            autocrop=autocrop)
+
+def copy_jpg_to_img(jpg_path, img, x_offset=None, y_offset=None, \
+                    autocrop=True):
+    img2 = pdb.gimp_file_load(jpg_path, jpg_path)
+    return copy_img2_to_img(img2, img, x_offset=x_offset, y_offset=y_offset, \
+                            autocrop=autocrop)
+
+def copy_png_to_img(png_path, img, x_offset=None, y_offset=None, \
+                    autocrop=True):
+    img2 = pdb.gimp_file_load(png_path, png_path)
+    return copy_img2_to_img(img2, img, x_offset=x_offset, y_offset=y_offset, \
+                            autocrop=autocrop)
 
 def load_outline_png():
     myname = 'outline1.png'
@@ -354,6 +371,7 @@ register(
 import tkFileDialog
 pngtypes = [('png files', '*.png')]
 pdftypes = [('pdf files', '*.pdf')]
+jpgtypes = [('jpg files', '*.jpg')]
 import tk_simple_dialog
 
 def open_png(initialdir=None, initialfile=None):
@@ -366,6 +384,12 @@ def open_pdf(initialdir=None, initialfile=None):
     filename = tkFileDialog.askopenfilename(initialfile=initialfile,
                                             initialdir=initialdir,
                                             filetypes=pdftypes)
+    return filename
+
+def open_jpg(initialdir=None, initialfile=None):
+    filename = tkFileDialog.askopenfilename(initialfile=initialfile,
+                                            initialdir=initialdir,
+                                            filetypes=jpgtypes)
     return filename
 
 def load_any_png():
@@ -411,6 +435,29 @@ register(
         [],
         [],
         load_any_pdf)
+    
+
+def load_any_jpg():
+    img = pdb.python_fu_new_grid_image()
+    jpgpath = open_jpg()
+    floating_sel = copy_jpg_to_img(jpgpath, img, x_offset=25, \
+                                   y_offset=25)
+##     if top_layer_is_TEMP(img, 1) or top_layer_is_Latex(img, 1):
+##         pdb.gimp_floating_sel_anchor(floating_sel)
+
+
+register(
+        "load_any_jpg",
+        "Load any jpg into file",
+        "Load jpg into file",
+        "Ryan Krauss",
+        "Ryan Krauss",
+        "2009",
+        "<Toolbox>/Xtns/Languages/Ryan/Latex/Load JPG",
+        "",#"RGB*, GRAY*",
+        [],
+        [],
+        load_any_jpg)
     
 
 def paste_eqn_with_offests_and_clear(img, drawable):
